165768e46b54edaf95c00733c8bf4936e30363b0
Giuseppe Rollo <info@rainingdreams.co.uk>
2022-12-02 08:47:46 +0000

    added day 2 part 1 and 2

---
+++ src/dotnet/Domain/Day2/Day2.cs
@@ -0,0 +1,76 @@
+namespace Domain.Day2;
+
+public static class Day2
+{
+    public static int GetTotalScore1(string data)
+    {
+        var rounds = data.Split("\n");
+        var totalScore1 = rounds.Select(GetMyPoints1).Sum();
+
+        return totalScore1;
+    }
+
+    public static int GetTotalScore2(string data)
+    {
+        var rounds = data.Split("\n");
+        var totalScore1 = rounds.Select(GetMyPoints2).Sum();
+
+        return totalScore1;
+    }
+    
+    private static int GetMyPoints1(string round)
+    {
+        var combinations = new Dictionary<string, int>()
+        {
+            {"A X", 3},
+            {"A Y", 6},
+            {"A Z", 0},
+            {"B X", 0},
+            {"B Y", 3},
+            {"B Z", 6},
+            {"C X", 6},
+            {"C Y", 0},
+            {"C Z", 3},
+        };
+
+        var handPoints = new Dictionary<string, int>()
+        {
+            {"X", 1},
+            {"Y", 2},
+            {"Z", 3},
+        };
+
+        var hand = round.Split(' ')[1];
+
+        return combinations[round] + handPoints[hand];
+    }
+    
+    private static int GetMyPoints2(string round)
+    {
+        var combinationsHandPoints = new Dictionary<string, int>()
+        {
+            { "A X", 3 },
+            { "A Y", 1 },
+            { "A Z", 2 },
+                
+            { "B X", 1 },
+            { "B Y", 2 },
+            { "B Z", 3 },
+                
+            { "C X", 2 },
+            { "C Y", 3 },
+            { "C Z", 1 },
+        };
+
+        var outcomePoints = new Dictionary<string, int>()
+        {
+            {"X", 0},
+            {"Y", 3},
+            {"Z", 6},
+        };
+            
+        var handPoints = round.Split(' ')[1];
+
+        return combinationsHandPoints[round] + outcomePoints[handPoints];
+    }
+}
---
+++ src/dotnet/UnitTests/Day2Tests.cs
@@ -0,0 +1,40 @@
+using Domain.Day2;
+
+namespace UnitTests;
+
+public class Day2Tests
+{
+    private readonly string _data;
+    
+    public Day2Tests()
+    {
+        const string testDataFilePath = @"./TestData/Day2_Data.txt";
+        _data =  File.ReadAllText(testDataFilePath);
+    }
+
+    [Fact]
+    public void GetTotalScore()
+    {
+        // Arrange
+        var expectedResult = 15;
+        
+        // Act
+        var result = Day2.GetTotalScore1(_data);
+        
+        // Assess
+        Assert.Equal(expectedResult, result);
+    }
+
+    [Fact]
+    public void GetActualTotalScore()
+    {
+        // Arrange
+        var expectedResult = 12;
+
+        // Act
+        var result = Day2.GetTotalScore2(_data);
+        
+        // Assess
+        Assert.Equal(expectedResult, result);
+    }
+}
